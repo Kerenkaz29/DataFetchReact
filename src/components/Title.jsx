12dd08ee6cb0c68f43f1a1db13a1837498ee5b8d
Kerenkaz29 <kerenkaza29@gmail.com>
2026-08-02 19:13:33 +0300

    FastFetch: Add a title to the website

---
+++ src/components/Title.jsx
@@ -0,0 +1,9 @@
+function Title() {
+  return (
+    <h1 className="text-8xl sm:text-9xl font-bold bg-linear-to-r from-fuchsia-400 via-purple-400 to-cyan-400 bg-clip-text text-transparent">
+      FastFetch
+    </h1>
+  )
+}
+
+export default Title
